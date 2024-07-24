f355ab8b623f0208a06cbcfe48c7ad4d342e6364
Jim Anderson <jmanderson@usf.edu>
2024-07-24 19:54:55 +0000

    First commit into repository

---
+++ fork.c
@@ -0,0 +1,27 @@
+/*
+ * Simple Fork Example
+ *
+ * Note: if needed, run from shell
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void forkexample()
+{
+    int PID;
+    // child process because return value zero
+    PID = fork();
+    if (PID == 0)
+        printf("Hello from Child!\n");
+  
+    // parent process because return value non-zero.
+    else
+        printf("Hello from Parent!\n");
+        printf("Child PID: %d\n",PID);
+}
+int main()
+{
+    forkexample();
+    return 0;
+}
